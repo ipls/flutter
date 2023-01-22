1b1acf4e40f52719ff9996344770ea9fced80651
ipls <andresaefullohxx@gmail.com>
2023-01-22 14:32:20 +0700

    done submit register form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar_no1/basic_screen.dart';
+// import 'package:belajar_no1/basic_screen.dart';
 import 'package:belajar_no1/register_screen.dart';
 import 'package:flutter/material.dart';
 
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  var nameController = TextEditingController();
+  var usernameController = TextEditingController();
+  var emailController = TextEditingController();
+  var passwordController = TextEditingController();
+  var addressController = TextEditingController();
+  var phoneNumberController = TextEditingController();
+  var nama = "";
+  var username = "";
+  var email = "";
+  var password = "";
+  var address = "";
+  var phoneNumber = "";
 
   @override
   Widget build(BuildContext context) {
@@ -14,23 +32,29 @@ class RegisterScreen extends StatelessWidget {
               children: [
                 Text("Silahkan Registrasi Dahulu"),
                 TextFormField(
+                  controller: nameController,
                   decoration: InputDecoration(label: Text("Nama Lengkap")),
                 ),
                 TextFormField(
+                  controller: usernameController,
                   decoration: InputDecoration(label: Text("Username")),
                 ),
                 TextFormField(
+                  controller: emailController,
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(label: Text("Email")),
                 ),
                 TextFormField(
+                  controller: passwordController,
                   obscureText: true,
                   decoration: InputDecoration(label: Text("Password")),
                 ),
                 TextFormField(
+                  controller: addressController,
                   decoration: InputDecoration(label: Text("Address")),
                 ),
                 TextFormField(
+                  controller: phoneNumberController,
                   keyboardType: TextInputType.phone,
                   decoration: InputDecoration(label: Text("Phone Number")),
                 ),
@@ -40,6 +64,31 @@ class RegisterScreen extends StatelessWidget {
                 TextFormField(
                   decoration: InputDecoration(label: Text("Etc")),
                 ),
+                SizedBox(height: 32,),
+                ElevatedButton(onPressed: (){
+                  setState(() {
+                    nama = nameController.text;
+                    username = usernameController.text;
+                    email = emailController.text;
+                    password = passwordController.text;
+                    address = addressController.text;
+                    phoneNumber = phoneNumberController.text;
+                    nameController.text = "";
+                    usernameController.text = "";
+                    emailController.text = "";
+                    passwordController.text = "";
+                    addressController.text = "";
+                    phoneNumberController.text = "";
+                  });
+                }, child: Text("Register")),
+                SizedBox(height: 32,),
+                Text("Hasil Submit Form"),
+                Text("Name: ${nama}"),
+                Text("UserName: ${username}"),
+                Text("Email: ${email}"),
+                Text("Password: ${password}"),
+                Text("Address: ${address}"),
+                Text("PhoneNumber: ${phoneNumber}"),
               ],
             )),
       ),),
