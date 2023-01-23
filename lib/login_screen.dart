cc7e7e91bab89465aea21934135710d00ab53523
ipls <andresaefullohxx@gmail.com>
2023-01-23 17:18:37 +0700

    done login screen check user account

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,4 +1,8 @@
+import 'package:belajar_no1/register_screen.dart';
 import 'package:flutter/material.dart';
+
+const usernameData = "Andre";
+const passwordData = "12345";
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -8,10 +12,110 @@ class LoginScreen extends StatefulWidget {
 }
 
 class LoginScreenState extends State<LoginScreen> {
+  var usernameController = TextEditingController();
+  var passwordController = TextEditingController();
+  var username = "";
+  var password = "";
+  bool isLoginSuccess = false;
+  bool isUserHasTriedLogin = false;
+  var formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Login Screen"),),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Login Screen"),),
+        body: Center(child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    const Text("Silahkan Login"),
+                    TextFormField(
+                      controller: usernameController,
+                      validator: (val){
+                        if (val?.isEmpty ?? false){
+                          return "Field ini tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        username = usernameController.text;
+                        usernameController.text = "";
+                      },
+                      decoration: const InputDecoration(label: Text("Username")),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      // validator: (value){
+                      //   if((value?.length ?? 0) < 8) {
+                      //     return "Password Tidak Boleh Kurang Dari 8 Karakter";
+                      //   }
+                      //   return null;
+                      // },
+                      onSaved: (val) {
+                        password = passwordController.text;
+                        passwordController.text = "";
+                      },
+                      obscureText: true,
+                      decoration: const InputDecoration(label: Text("Password")),
+                    ),
+                    const SizedBox(height:32,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Belum Punga Akun? ",),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).push(
+                                MaterialPageRoute(
+                                builder: (context) => const RegisterScreen(),
+                                ),
+                            );
+                          },
+                          child: const Text("Daftar Disini.",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              decoration: TextDecoration.underline,
+                            ),),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: 32,),
+                    ElevatedButton(onPressed: () {
+                      if ((formKey.currentState?.validate() ?? false)) {
+                        setState(() {
+                          formKey.currentState?.save();
+                          isUserHasTriedLogin = true;
+                          if(username == usernameData && password == passwordData){
+                            isLoginSuccess = true;
+                            } else {
+                            isLoginSuccess = false;
+                          }
+                        });
+                      }
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (context) => const LoginScreen(),
+                      //   ),
+                      // );
+                    },
+                        child: const Text("Login")),
+                    const SizedBox(height: 32,),
+                    Visibility(
+                        visible: isUserHasTriedLogin && isLoginSuccess,
+                        child: const Text("Login Berhasil")),
+                    Visibility(
+                        visible: isUserHasTriedLogin && !isLoginSuccess,
+                        child: const Text("Login Gagal"))
+                  ],
+                )),
+          ),
+        ),),
+      ),
     );
   }
 }
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'login_screen.dart';
+// import 'login_screen.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -26,135 +26,131 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Register Screen"),),
-      body: Center(child: SingleChildScrollView(
-        child: Form(
-          key: formKey,
-          child: Container(
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                children: [
-                  Text("Silahkan Registrasi Dahulu"),
-                  TextFormField(
-                    controller: nameController,
-                    validator: (value){
-                      if((value?.length ?? 0) > 5) {
-                        return "Nama Tidak Boleh Lebih Dari 5 Karakter";
-                      }
-                      return null;
-                    },
-                    decoration: InputDecoration(label: Text("Nama Lengkap")),
-                    onSaved: (val) {
-                      nama = nameController.text;
-                      nameController.text = "";
-                    },
-                  ),
-                  TextFormField(
-                    controller: usernameController,
-                    validator: (val){
-                      if (val?.isEmpty ?? false){
-                        return "Field ini tidak boleh kosong";
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                      username = usernameController.text;
-                      usernameController.text = "";
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text("Register Screen"),),
+        body: Center(child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    Text("Silahkan Registrasi Dahulu"),
+                    TextFormField(
+                      controller: nameController,
+                      validator: (value){
+                        if((value?.length ?? 0) > 5) {
+                          return "Nama Tidak Boleh Lebih Dari 5 Karakter";
+                        }
+                        return null;
                       },
-                    decoration: InputDecoration(label: Text("Username")),
-                  ),
-                  TextFormField(
-                    controller: emailController,
-                    validator: (val){
-                      if (val?.isEmpty ?? false){
-                        return "Field ini tidak boleh kosong";
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                      email = emailController.text;
-                      emailController.text = "";
+                      decoration: InputDecoration(label: Text("Nama Lengkap")),
+                      onSaved: (val) {
+                        nama = nameController.text;
+                        nameController.text = "";
                       },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(label: Text("Email")),
-                  ),
-                  TextFormField(
-                    controller: passwordController,
-                    validator: (value){
-                      if((value?.length ?? 0) < 8) {
-                        return "Nama Tidak Boleh Kurang Dari 8 Karakter";
+                    ),
+                    TextFormField(
+                      controller: usernameController,
+                      validator: (val){
+                        if (val?.isEmpty ?? false){
+                          return "Field ini tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        username = usernameController.text;
+                        usernameController.text = "";
+                        },
+                      decoration: InputDecoration(label: Text("Username")),
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      validator: (val){
+                        if (val?.isEmpty ?? false){
+                          return "Field ini tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        email = emailController.text;
+                        emailController.text = "";
+                        },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(label: Text("Email")),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      validator: (value){
+                        if((value?.length ?? 0) < 8) {
+                          return "Nama Tidak Boleh Kurang Dari 8 Karakter";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                        password = passwordController.text;
+                        passwordController.text = "";
+                      },
+                      obscureText: true,
+                      decoration: InputDecoration(label: Text("Password")),
+                    ),
+                    TextFormField(
+                      controller: addressController,
+                      validator: (val) {
+                        if (val?.isEmpty ?? false){
+                          return "Field ini tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      onSaved: (val){
+                        address = addressController.text;
+                        addressController.text = "";
+                      },
+                      decoration: InputDecoration(label: Text("Address")),
+                    ),
+                    TextFormField(
+                      controller: phoneNumberController,
+                      validator: (val) {
+                        if (val?.isEmpty ?? false){
+                          return "Field ini tidak boleh kosong";
+                        }
+                        return null;
+                      },
+                      onSaved: (val) {
+                      phoneNumber = phoneNumberController.text;
+                      phoneNumberController.text = "";
+                      },
+                      keyboardType: TextInputType.phone,
+                      decoration: InputDecoration(label: Text("Phone Number")),
+                    ),
+                    SizedBox(height: 32,),
+                    ElevatedButton(onPressed: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //     builder: (context) => const LoginScreen(),
+                      //   ),
+                      // );
+                      if ((formKey.currentState?.validate() ?? false)) {
+                        setState(() {
+                          formKey.currentState?.save();
+                        });
                       }
-                      return null;
                     },
-                    onSaved: (val) {
-                      password = passwordController.text;
-                      passwordController.text = "";
-                    },
-                    obscureText: true,
-                    decoration: InputDecoration(label: Text("Password")),
-                  ),
-                  TextFormField(
-                    controller: addressController,
-                    validator: (val) {
-                      if (val?.isEmpty ?? false){
-                        return "Field ini tidak boleh kosong";
-                      }
-                      return null;
-                    },
-                    onSaved: (val){
-                      address = addressController.text;
-                      addressController.text = "";
-                    },
-                    decoration: InputDecoration(label: Text("Address")),
-                  ),
-                  TextFormField(
-                    controller: phoneNumberController,
-                    validator: (val) {
-                      if (val?.isEmpty ?? false){
-                        return "Field ini tidak boleh kosong";
-                      }
-                      return null;
-                    },
-                    onSaved: (val) {
-                    phoneNumber = phoneNumberController.text;
-                    phoneNumberController.text = "";
-                    },
-                    keyboardType: TextInputType.phone,
-                    decoration: InputDecoration(label: Text("Phone Number")),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(label: Text("Etc")),
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(label: Text("Etc")),
-                  ),
-                  SizedBox(height: 32,),
-                  ElevatedButton(onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const LoginScreen(),
-                      ),
-                    );
-                    // if ((formKey.currentState?.validate() ?? false)) {
-                    //   setState(() {
-                    //     formKey.currentState?.save();
-                    //   });
-                    // }
-                  },
-                      child: Text("Register")),
-                  SizedBox(height: 32,),
-                  Text("Hasil Submit Form"),
-                  Text("Name: ${nama}"),
-                  Text("UserName: ${username}"),
-                  Text("Email: ${email}"),
-                  Text("Password: ${password}"),
-                  Text("Address: ${address}"),
-                  Text("PhoneNumber: ${phoneNumber}"),
-                ],
-              )),
-        ),
-      ),),
+                        child: Text("Register")),
+                    SizedBox(height: 32,),
+                    Text("Hasil Submit Form"),
+                    Text("Name: ${nama}"),
+                    Text("UserName: ${username}"),
+                    Text("Email: ${email}"),
+                    Text("Password: ${password}"),
+                    Text("Address: ${address}"),
+                    Text("PhoneNumber: ${phoneNumber}"),
+                  ],
+                )),
+          ),
+        ),),
+      ),
     );
   }
 }
