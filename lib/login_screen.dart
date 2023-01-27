e1b73f6da873d89ca35a216ed38f265e14d7d3f5
ipls <andresaefullohxx@gmail.com>
2023-01-27 21:11:51 +0700

    done login screen check user account

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,7 @@
+import 'package:belajar_no1/basic_screen.dart';
 import 'package:belajar_no1/register_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 const usernameData = "Andre";
 const passwordData = "12345";
@@ -20,102 +22,121 @@ class LoginScreenState extends State<LoginScreen> {
   bool isUserHasTriedLogin = false;
   var formKey = GlobalKey<FormState>();
 
+
+  Future<void> setIsLogin() async{
+    var prefs = await SharedPreferences.getInstance();
+    prefs.setBool("isLogin", isLoginSuccess);
+    var savedValue = prefs.getBool("isLogin");
+    print("savedValue $savedValue");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(title: const Text("Login Screen"),),
-        body: Center(child: SingleChildScrollView(
-          child: Form(
-            key: formKey,
-            child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    const Text("Silahkan Login"),
-                    TextFormField(
-                      controller: usernameController,
-                      validator: (val){
-                        if (val?.isEmpty ?? false){
-                          return "Field ini tidak boleh kosong";
-                        }
-                        return null;
-                      },
-                      onSaved: (val) {
-                        username = usernameController.text;
-                        usernameController.text = "";
-                      },
-                      decoration: const InputDecoration(label: Text("Username")),
-                    ),
-                    TextFormField(
-                      controller: passwordController,
-                      // validator: (value){
-                      //   if((value?.length ?? 0) < 8) {
-                      //     return "Password Tidak Boleh Kurang Dari 8 Karakter";
-                      //   }
-                      //   return null;
-                      // },
-                      onSaved: (val) {
-                        password = passwordController.text;
-                        passwordController.text = "";
-                      },
-                      obscureText: true,
-                      decoration: const InputDecoration(label: Text("Password")),
-                    ),
-                    const SizedBox(height:32,),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
+        return SafeArea(
+          child: Scaffold(
+            appBar: AppBar(title: const Text("Login Screen"),),
+            body: Center(child: SingleChildScrollView(
+              child: Form(
+                key: formKey,
+                child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
                       children: [
-                        const Text("Belum Punga Akun? ",),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).push(
-                                MaterialPageRoute(
-                                builder: (context) => const RegisterScreen(),
-                                ),
-                            );
+                        const Text("Silahkan Login"),
+                        TextFormField(
+                          controller: usernameController,
+                          validator: (val){
+                            if (val?.isEmpty ?? false){
+                              return "Field ini tidak boleh kosong";
+                            }
+                            return null;
                           },
-                          child: const Text("Daftar Disini.",
-                            style: TextStyle(
-                              color: Colors.blue,
-                              decoration: TextDecoration.underline,
-                            ),),
+                          onSaved: (val) {
+                            username = usernameController.text;
+                            usernameController.text = "";
+                          },
+                          decoration: const InputDecoration(label: Text("Username")),
                         ),
-                      ],
-                    ),
+                        TextFormField(
+                          controller: passwordController,
+                          // validator: (value){
+                          //   if((value?.length ?? 0) < 8) {
+                          //     return "Password Tidak Boleh Kurang Dari 8 Karakter";
+                          //   }
+                          //   return null;
+                          // },
+                          obscureText: true,
+                          onSaved: (val) {
+                            password = passwordController.text;
+                            passwordController.text = "";
+                          },
+                          decoration: const InputDecoration(label: Text("Password")),
+                        ),
+                        const SizedBox(height:32,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Text("Belum Punga Akun? ",),
+                            GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                    MaterialPageRoute(
+                                    builder: (context) => const RegisterScreen(),
+                                    ),
+                                );
+                              },
+                              child: Text("Daftar Disini.",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                ),),
+                            ),
+                          ],
+                        ),
 
-                    const SizedBox(height: 32,),
-                    ElevatedButton(onPressed: () {
-                      if ((formKey.currentState?.validate() ?? false)) {
-                        setState(() {
-                          formKey.currentState?.save();
-                          isUserHasTriedLogin = true;
-                          if(username == usernameData && password == passwordData){
-                            isLoginSuccess = true;
+                        const SizedBox(height: 32,),
+                        ElevatedButton(onPressed: () async {
+                          if ((formKey.currentState?.validate() ?? false)) {
+                            formKey.currentState?.save();
+
+                            setState(() {
+                              isUserHasTriedLogin = true;
+                            });
+
+                            if(username == usernameData && password == passwordData){
+                              setState((){
+                                isLoginSuccess = true;
+                              });
+                              await setIsLogin();
+                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const BasicScreen(),
+                              ),
+                              );
                             } else {
-                            isLoginSuccess = false;
+                              setState(() {
+                                isLoginSuccess = false;
+                              });
+                            }
                           }
-                        });
-                      }
-                      // Navigator.of(context).push(
-                      //   MaterialPageRoute(
-                      //     builder: (context) => const LoginScreen(),
-                      //   ),
-                      // );
-                    },
-                        child: const Text("Login")),
-                    const SizedBox(height: 32,),
-                    Visibility(
-                        visible: isUserHasTriedLogin && isLoginSuccess,
-                        child: const Text("Login Berhasil")),
-                    Visibility(
-                        visible: isUserHasTriedLogin && !isLoginSuccess,
-                        child: const Text("Login Gagal"))
-                  ],
-                )),
+                          // Navigator.of(context).push(
+                          //   MaterialPageRoute(
+                          //     builder: (context) => const LoginScreen(),
+                          //   ),
+                          // );
+                        },
+                            child: const Text("Login")),
+                        const SizedBox(height: 32,),
+                        Visibility(
+                            visible: isUserHasTriedLogin && isLoginSuccess,
+                            child: const Text("Login Berhasil")),
+                        Visibility(
+                            visible: isUserHasTriedLogin && !isLoginSuccess,
+                            child: const Text("Login Gagal"))
+                      ],
+                    )),
+              ),
+            ),),
           ),
-        ),),
-      ),
-    );
+        );
+      }
   }
-}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,58 @@
 // import 'package:belajar_no1/basic_screen.dart';
+import 'package:belajar_no1/basic_screen.dart';
 import 'package:belajar_no1/login_screen.dart';
-import 'package:belajar_no1/register_screen.dart';
 import 'package:flutter/material.dart';
+// import 'package:belajar_no1/register_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  bool isUserHasLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    SharedPreferences.getInstance().then((prefValue) =>
+    {
+      setState(() {
+        isUserHasLogin = prefValue.getBool("isLogin") ?? false;
+        print("isUserHasLogin: $isUserHasLogin");
+      })
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const LoginScreen(),
-    );
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: isUserHasLogin ? const BasicScreen() : const LoginScreen(),
+        );
+      }
   }
-}
+
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
